25ad9d893071a1c56461f952f5b0a9cf47931294
Rain Zhang <tza103@sfu.ca>
2026-03-09 16:03:31 -0700

    Export intraday validation APIs

--- src/stocktradebot/models/__init__.py
+++ src/stocktradebot/models/__init__.py
@@ -1,7 +1,9 @@
+from stocktradebot.models.intraday import validate_intraday_research
 from stocktradebot.models.service import backtest_model, model_status, train_model
 from stocktradebot.models.types import (
     BacktestRunSummary,
     DatasetArtifactRow,
+    IntradayValidationSummary,
     LinearModelArtifact,
     TrainingRunSummary,
     ValidationRunSummary,
@@ -10,10 +12,12 @@ from stocktradebot.models.types import (
 __all__ = [
     "BacktestRunSummary",
     "DatasetArtifactRow",
+    "IntradayValidationSummary",
     "LinearModelArtifact",
     "TrainingRunSummary",
     "ValidationRunSummary",
     "backtest_model",
     "model_status",
     "train_model",
+    "validate_intraday_research",
 ]
